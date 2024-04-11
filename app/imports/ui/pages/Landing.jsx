58f3d7ac14f6f6248674e97f97c5e173184c01b4
John Serraon <jserraon@hawaii.edu>
2024-04-11 01:13:36 -1000

    Landing page touch-up
    Added some CSS styling for attractiveness

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import { Button, Col, Container, Image, Nav, Row } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
@@ -10,11 +11,14 @@ const Landing = () => (
         <Image roundedCircle src="/images/rainbow-cards-icon.png" width="200px" />
       </Col>
 
-      <Col className="d-flex flex-column justify-content-center greentxt">
-        <h1>Mānoa Rainbow Cards</h1>
+      <Col className="d-flex flex-column justify-content-center align-items-center greentxt">
+        <h1 className="font-effect-fire-animation" id="landing-title">Mānoa<br />Rainbow Cards</h1>
         {/* eslint-disable-next-line max-len */}
         <p>Rainbow Cards is a card collection site where you can collect the cards of your favorite professors. Each card includes information and fun facts about the professor. Aim to collect a variety of cards and trade them for your desired cards with other users!
         </p>
+        <Button id="sign-up-button" as={NavLink} to="/signup" key="signup">Get Started</Button>
+        <br />
+        <p>Want to contribute? <a href="https://forms.gle/RJjyfaoXvakZ6eQNA">Opt-In Form for Professors</a></p>
       </Col>
 
       <Col xs={4}>
