d99e6b321476012d8de935a6798b491cec4925f5
elijah-saloma <esaloma@hawaii.edu>
2024-04-18 12:27:14 -1000

    Implemented 6 tests
     ✓ Test that landing page shows up
     ✓ Test that signin and signout work
     ✓ Test that catalog and mycards work for generic accounts
     ✓ Test that signin and signout work for admin accounts
     ✓ Test that catalog and mycards work for admin accounts

---
+++ app/tests/add.page.js
@@ -0,0 +1,33 @@
+import { Selector } from 'testcafe';
+
+class AddPage {
+  constructor() {
+    this.pageId = '#add-card-admin-nav';
+    this.pageSelector = Selector(this.pageId);
+    // this.submitButton = Selector('#add-submit');
+  }
+
+  /** Asserts that this page is currently displayed. */
+  async isDisplayed(testController) {
+    await testController.expect(this.pageSelector.exists).ok();
+  }
+
+  async add(testController, name, course, semester, department, email, image, facts, campuseats /* ,hiddentalent */) {
+    await this.isDisplayed(testController);
+    await testController.typeText('#name', name);
+    await testController.typeText('#course', course);
+    await testController.typeText('#semester', semester);
+    await testController.typeText('#department', department);
+    await testController.typeText('#email', email);
+    await testController.typeText('#image', image);
+    await testController.typeText('#facts', facts);
+    await testController.typeText('#campuseats', campuseats);
+    // await testController.typeText('#hiddentalent', hiddentalent);
+    await testController.click('#add-submit input.btn.btn-primary');
+    await testController.click('#');
+    // Below line doesn't work for some reason
+    // await testController.click(submitButton());
+  }
+}
+
+export const addPage = new AddPage();
