85a2ff2b24cfef8ff793be4116b34b3886c2da26
John Serraon <jserraon@hawaii.edu>
2024-04-18 20:01:57 -1000

    Updated DevTest to pull from ProfCard publication

--- app/imports/ui/pages/DevTest.jsx
+++ app/imports/ui/pages/DevTest.jsx
@@ -1,69 +1,42 @@
 import React from 'react';
+import { Meteor } from 'meteor/meteor';
 import { Card, Col, Container, Row } from 'react-bootstrap';
 import { AutoForm, ErrorsField, SubmitField, TextField, SelectField } from 'uniforms-bootstrap5';
+import { useTracker } from 'meteor/react-meteor-data';
 import swal from 'sweetalert';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import SimpleSchema from 'simpl-schema';
-import { Meteor } from 'meteor/meteor';
-
-const AllCards = [
-  {
-    name: 'Philip Johnson',
-    course: 'ICS314',
-    semester: 'Spring 2024',
-    department: 'ICS',
-    email: 'pjohnson@hawaii.edu',
-    image: 'https://github.com/philipmjohnson.png',
-    facts: 'I am a Professor of Information and Computer Sciences at the University of Hawaii, Director ' +
-      'of the Collaborative Software Development Laboratory, and the CEO of OpenPowerQuality.com.',
-    campusEats: 'Panda',
-  },
-  {
-    name: 'Henri Casanova',
-    course: 'ICS111',
-    semester: 'Spring 2024',
-    department: 'ICS',
-    email: 'pjohnson@hawaii.edu',
-    image: 'https://avatars0.githubusercontent.com/u/7494478?s=460&v=4',
-    facts: 'I am a Professor of Information and Computer Sciences at the University of Hawaii, Director ' +
-      'of the Collaborative Software Development Laboratory, and the CEO of OpenPowerQuality.com.',
-    campusEats: 'Panda',
-  },
-  {
-    name: 'Kim Binsted',
-    course: 'ICS212',
-    semester: 'Spring 2024',
-    department: 'ICS',
-    email: 'pjohnson@hawaii.edu',
-    image: 'https://www.ics.hawaii.edu/wp-content/uploads/2013/08/kim_binsted-square-300x300.jpg',
-    facts: 'I am a Professor of Information and Computer Sciences at the University of Hawaii, Director ' +
-      'of the Collaborative Software Development Laboratory, and the CEO of OpenPowerQuality.com.',
-    campusEats: 'Subway',
-  },
-  {
-    name: 'Kim Binsted',
-    course: 'ICS212',
-    semester: 'Fall 2023',
-    department: 'ICS',
-    email: 'pjohnson@hawaii.edu',
-    image: 'https://www.ics.hawaii.edu/wp-content/uploads/2013/08/kim_binsted-square-300x300.jpg',
-    facts: 'I am a Professor of Information and Computer Sciences at the University of Hawaii, Director ' +
-      'of the Collaborative Software Development Laboratory, and the CEO of OpenPowerQuality.com.',
-    campusEats: 'L&L Hawaiian BBQ',
-  },
-];
-
-// Create a schema to specify the structure of the data to appear in the form.
-const formSchema = new SimpleSchema({
-  card: { label: 'Select a card', type: String, allowedValues: AllCards.map(card => `${card.name} (${card.semester}, ${card.course})`) },
-  user: { label: 'Enter a user', type: String },
-});
-const subscriptionUsernames = Meteor.subscribe('allUsernames');
-const bridge = new SimpleSchema2Bridge(formSchema);
+import { ProfCards } from '../../api/profcard/ProfCard';
+import LoadingSpinner from '../components/LoadingSpinner';
 
 /* Renders the DevTest page section for sending a card to a user. */
 const DevTest = () => {
   // On submit, insert the data.
+  // Create a schema to specify the structure of the data to appear in the form.
+  const { profcards, ready } = useTracker(() => {
+    // Note that this subscription will get cleaned up
+    // when your component is unmounted or deps change.
+    // Get access to ProfCards documents.
+    const subscription = Meteor.subscribe('cards.public');
+    // Determine if the subscription is ready
+    const rdy = subscription.ready();
+    // Get the ProfCards documents
+    const profCardItems = ProfCards.collection.find().fetch();
+    return {
+      profcards: profCardItems,
+      ready: rdy,
+    };
+  }, []);
+
+  const subscriptionUsernames = Meteor.subscribe('allUsernames');
+
+  // Create a schema to specify the structure of the data to appear in the form.
+  const formSchema = new SimpleSchema({
+    card: { label: 'Select a card', type: String, allowedValues: profcards.map(card => `${card.name} (${card.semester}, ${card.course})`) },
+    user: { label: 'Enter a user', type: String },
+  });
+  const bridge = new SimpleSchema2Bridge(formSchema);
+
   const submit = (giftData, formRef) => {
     const { card, user } = giftData;
     // Find the user by their username via string
@@ -91,7 +64,7 @@ const DevTest = () => {
 
   // Render the form. Use Uniforms: https://github.com/vazco/uniforms
   let fRef = null;
-  return (
+  return (ready ? (
     <Container className="py-3">
       <Row className="justify-content-center">
         <Col xs={7}>
@@ -128,7 +101,7 @@ const DevTest = () => {
         </Col>
       </Row>
     </Container>
-  );
+  ) : <LoadingSpinner />);
 };
 
 export default DevTest;
