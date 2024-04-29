8c2f46a5f1f47acda93929e1ac6fd30e23760ed4
John Serraon <jserraon@hawaii.edu>
2024-04-29 02:10:46 -1000

    Enhanced the site footer

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,27 +1,69 @@
 import React from 'react';
-import { Col, Container } from 'react-bootstrap';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const Footer = () => (
-  <footer className="mt-auto py-3 bg-success-subtle">
-    <Container>
-      <Col className="text-center greentxt">
-        Mānoa Rainbow Cards
-        {' '}
-        <br />
-        University of Hawaii
-        <br />
-        Honolulu, HI 96822
-        {' '}
-        <br />
-        <a href="https://rainbow-cards.github.io/">
-          Project Homepage
-        </a>
-        <br />
-        Daevin Bagcal, Benjamin Bercasio, Yuzuki Fujimoto, Elijah Saloma, John Serraon
-      </Col>
+  <footer className="mt-auto py-3 navbar-custom">
+    <Container className="d-flex justify-content-center align-items-center">
+      <Card className="site-footer-plate" style={{ minWidth: '1000px', maxWidth: '1000px' }}>
+        <Col className="text-center greentxt green-highlight">
+          <Card.Header>
+            <Row>
+              <Col>
+                About Us
+              </Col>
+              <Col>
+                Rainbow Cards Team
+              </Col>
+              <Col>
+                Resources
+              </Col>
+            </Row>
+          </Card.Header>
+          <Row>
+            <Col>
+              <a href="https://manoa.hawaii.edu/">
+                University of Hawaii at Mānoa
+              </a>
+              <br />
+              Honolulu, HI 96822
+              {' '}
+              <br />
+              ICS 314, Spring 2024
+            </Col>
+            <Col>
+              <Row>
+                <Col className="footer-column-gap">
+                  Daevin Bagcal<br />
+                  Benjamin Bercasio<br />
+                  Yuzuki Fujimoto
+                </Col>
+                <Col className="footer-column-gap">
+                  Elijah Saloma<br />
+                  John Serraon
+                </Col>
+              </Row>
+            </Col>
+            <Col className="footer-column-gap">
+              <a href="https://rainbow-cards.github.io/">
+                Project Homepage
+              </a>
+              <br />
+              <a href="https://github.com/rainbow-cards">
+                GitHub Organization
+              </a>
+              <br />
+              <a href="https://courses.ics.hawaii.edu/ics314s24/index.html">
+                Course Website
+              </a>
+              <br />
+            </Col>
+          </Row>
+        </Col>
+      </Card>
     </Container>
   </footer>
+
 );
 
 export default Footer;
