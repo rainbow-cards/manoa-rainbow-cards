a424bc670733d55413f25d148ea992b928519ab9
John Serraon <jserraon@hawaii.edu>
2024-04-30 15:30:18 -1000

    Update CardSearchIndex.js

--- app/imports/startup/server/CardSearchIndex.js
+++ app/imports/startup/server/CardSearchIndex.js
@@ -1,9 +1,13 @@
 import { Meteor } from 'meteor/meteor';
+<<<<<<< Updated upstream
 import { ProfCards } from '/path/to/ProfCards.js'; // Replace '/path/to/ProfCards.js' with the correct path
+=======
+import { ProfCard } from '../api/profcard/ProfCard.js';
+>>>>>>> Stashed changes
 
 Meteor.startup(() => {
   // Create a unique index on the server
-  ProfCards.collection.rawCollection().createIndex(
+  ProfCard.collection.rawCollection().createIndex(
     { name: 1, course: 1, semester: 1 },
     { unique: true },
   );
