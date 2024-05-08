71637ed7cfad33b2d0be157e1464bfc6ef9212a7
John Serraon <jserraon@hawaii.edu>
2024-05-07 17:42:38 -1000

    Updated card distributions
    Can send multiple cards at once. Improved card selection reactability and feedback

--- app/imports/startup/server/CardSearchIndex.js
+++ app/imports/startup/server/CardSearchIndex.js
@@ -1,9 +1,9 @@
 import { Meteor } from 'meteor/meteor';
-import { ProfCard } from '../api/profcard/ProfCard.js';
+import { ProfCards } from '../../api/profcard/ProfCard.js';
 
 Meteor.startup(() => {
   // Create a unique index on the server
-  ProfCard.collection.rawCollection().createIndex(
+  ProfCards.collection.rawCollection().createIndex(
     { name: 1, course: 1, semester: 1 },
     { unique: true },
   );
