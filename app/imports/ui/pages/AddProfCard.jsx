a5eb50d1b9ece8b058695be504d28ec21132665c
daevinbag <daevinbag@gmail.com>
2024-05-08 21:16:36 -1000

    fixed the delete

--- app/imports/ui/pages/AddProfCard.jsx
+++ app/imports/ui/pages/AddProfCard.jsx
@@ -27,27 +27,23 @@ const AddProfCard = () => {
   const submit = (data, formRef) => {
     const { name, course, semester, department, email, image, facts, campusEats, hiddenTalent } = data;
     const owners = [{ name: email, count: 1 }];
-    try {
-      if (ProfCards.collection.find({ name: name, course: course, semester: semester }).count() === 0) {
-        ProfCards.collection.insert(
-          { name, course, semester, department, email, image, facts, owners, campusEats, hiddenTalent },
-          (error) => {
-            if (error) {
-              // console.log(ProfCards.collection.find({ name: name, course: course, semester: semester }));
-              swal('Error', 'A Rainbow Card for this course already exists.\n' +
-                'Check that the combination of name, course number, and semester does not already exist.', 'error');
-            } else {
-              swal('Success', 'Rainbow Card added successfully!', 'success');
-              formRef.reset();
-            }
-          },
-        );
-      }
-    } catch (error) {
-      console.error('Error: ', error);
+    const wished = [];
+    if (ProfCards.collection.find({ name: name, course: course, semester: semester }).count() === 0) {
+      ProfCards.collection.insert(
+        { name, course, semester, department, email, image, facts, owners, wished, campusEats, hiddenTalent },
+        (error) => {
+          if (error) {
+            // console.log(ProfCards.collection.find({ name: name, course: course, semester: semester }));
+            swal('Error', 'A Rainbow Card for this course already exists.\n' +
+              'Check that the combination of name, course number, and semester does not already exist.', 'error');
+          } else {
+            swal('Success', 'Rainbow Card added successfully!', 'success');
+            formRef.reset();
+          }
+        },
+      );
     }
   };
-
   // Render the form. Use Uniforms: https://github.com/vazco/uniforms
   let fRef = null;
   return (
